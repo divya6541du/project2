73d136e3df04202342df4a2a21c271295dba802a
divya6541du <divya6541du@gmail.com>
2026-01-02 09:47:44 +0530

    new

---
+++ day36 arr problem'/script.js
@@ -0,0 +1,65 @@
+//  Write a JavaScript function to get the first element of an array. Passing the parameter 'n' will return the first 'n' elements of the array.
+// Test Data :
+// console.log(first([7, 9, 0, -2]));
+// console.log(first([],3));
+// console.log(first([7, 9, 0, -2],3));
+// console.log(first([7, 9, 0, -2],6));
+// console.log(first([7, 9, 0, -2],-3));
+// Expected Output :
+// 7
+// []
+// [7, 9, 0]
+// [7, 9, 0, -2]
+// []
+
+
+
+
+
+function first(arr,n){
+     let emp=[]
+    if(n==undefined){
+        return arr[0]
+    }else{
+        let end=n
+        if(n>arr.length)
+        {
+           end=arr.length
+        }
+
+       for(let i=0;i<=end-1;i++)
+       {
+        emp.push(arr[i])
+       }
+       return emp
+     
+    }
+
+
+    // console.log(arr)
+    // console.log(n)
+ 
+}
+
+
+let ans=first([],3)
+// let ans=first(["a","b","c","d"],10)
+
+// console.log(ans)
+
+// 4. Write a JavaScript function to get the last element of an array. Passing the parameter 'n' will return the last 'n' elements of the array.
+// Test Data :
+// console.log(last([7, 9, 0, -2]));
+// console.log(last([7, 9, 0, -2],3));
+// console.log(last([7, 9, 0, -2],6));
+// Expected Output :
+// -2
+// [9, 0, -2]
+// [7, 9, 0, -2]
+
+// 5. Write a simple JavaScript program to join all elements of the following array into a string.
+// Sample array : myColor = ["Red", "Green", "White", "Black"];
+// Expected Output :
+// "Red,Green,White,Black"
+// "Red,Green,White,Black"
+// "Red+Green+White+Black"
